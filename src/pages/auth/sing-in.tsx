040391f1100b4052b903da7a613c46758b33afac
Robson Natanael <natanaelrobson@gmail.com>
2024-01-05 11:47:17 -0300

    feat: sonner configured

--- src/pages/auth/sing-in.tsx
+++ src/pages/auth/sing-in.tsx
@@ -3,6 +3,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Helmet } from "react-helmet-async"
 import { useForm } from "react-hook-form"
+import { toast } from "sonner"
 import { z } from "zod"
 
 const signInForm = z.object({
@@ -15,9 +16,21 @@ export function SignIn() {
   const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>()
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data);
-
-    await new Promise((resolve) => setTimeout(resolve, 2000))    
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+      
+      toast.success('Enviamos um link de autenticação para seu e-mail', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => {
+            handleSignIn(data)
+          }
+        }
+      })
+      
+    } catch (error) {
+      toast.error('Credenciais inválidas.')
+    }
   }
 
   return (
