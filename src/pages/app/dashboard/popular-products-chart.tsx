5dc43612dc555262dff94787f82e2765faf0da85
Robson Natanael <natanaelrobson@gmail.com>
2024-02-02 18:14:12 -0300

    feat(loading): created graphics loading

--- src/pages/app/dashboard/popular-products-chart.tsx
+++ src/pages/app/dashboard/popular-products-chart.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { BarChart } from "lucide-react"
+import { BarChart, Loader2 } from "lucide-react"
 import colors from "tailwindcss/colors"
 import { ResponsiveContainer, PieChart, Pie, Cell } from "recharts"
 import { useQuery } from "@tanstack/react-query"
@@ -30,7 +30,7 @@ export function PopularProductsChart() {
         </div>
       </CardHeader>
       <CardContent>
-        {popularProducts && (
+        {popularProducts ? (
           <ResponsiveContainer width="100%" height={240}>
             <PieChart>
               <Pie
@@ -85,6 +85,10 @@ export function PopularProductsChart() {
               </Pie>
             </PieChart>
           </ResponsiveContainer>
+        ) : (
+          <div className="flex h-[240px] w-full items-center justify-center">
+            <Loader2 className="size-8 animate-spin text-muted-foreground" />
+          </div>
         )}
       </CardContent>
     </Card>
